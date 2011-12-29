57b5f9ce25ef25cab7091a1da66d07cda84e3f48
Iain Urquhart <shout@iain.co.nz>
2011-12-30 12:14:59 +1300

    add some comments about modifying tag params

--- system/expressionengine/third_party/nested/libraries/nested_entries.php
+++ system/expressionengine/third_party/nested/libraries/nested_entries.php
@@ -24,6 +24,12 @@ class Nested_entries extends Channel {
      * @return string 
      */
     public function entries() {
+    	
+    	// we can do a bunch of stuff here to modify the nested:entries tag too, 
+    	// for example fetch child entries of the parent entry in Taxonomy *cough*
+    	
+    	// $this->EE->TMPL->tagparams['fixed_order'] = $child_ids;
+    	// $this->EE->TMPL->tagparams['limit'] = '1';
 
 		return parent::entries();
 
